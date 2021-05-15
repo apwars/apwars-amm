d09901daca62d726c599a9b4ebe639ddf6a9fce1
orc-tzan <>
2021-05-15 18:49:52 -0300

    menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,19 +31,31 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Gold Mine',
-        href: `${APWARS_URL}/farms/tier/0`,
+        href: `${APWARS_URL}/farms/tier/0/All`,
+      },
+      {
+        label: 'Gold Mine - Barracks',
+        href: `${APWARS_URL}/farms/tier/0/Barracks`,
+      },
+      {
+        label: 'Gold Mine - Armory',
+        href: `${APWARS_URL}/farms/tier/0/Armory`,
+      },
+      {
+        label: 'Gold Mine - Arcane Library',
+        href: `${APWARS_URL}/farms/tier/0/ArcaneLibrary`,
       },
       {
         label: 'Barracks',
-        href: `${APWARS_URL}/farms/tier/1`,
+        href: `${APWARS_URL}/farms/tier/1/All`,
       },
       {
         label: 'Armory',
-        href: `${APWARS_URL}/farms/tier/2`,
+        href: `${APWARS_URL}/farms/tier/2/All`,
       },
       {
         label: 'Arcane Library',
-        href: `${APWARS_URL}/farms/tier/3`,
+        href: `${APWARS_URL}/farms/tier/3/All`,
       },
     ],
   },
